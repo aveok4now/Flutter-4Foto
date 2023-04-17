73026761d472a063c88a030cd64d7d8146202089
dtb4life <aveok4now@gmail.com>
2023-04-17 18:46:48 +0300

    Chat-screen redesign + copy issue

--- lib/chatscreen.dart
+++ lib/chatscreen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(ChatbotApp());
 
+//https://cameralabs.org/uchebnik/osnovi-fotografii
 class ChatbotApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -20,6 +23,8 @@ class ChatbotScreen extends StatefulWidget {
 
 class _ChatbotScreenState extends State<ChatbotScreen> {
   final TextEditingController _controller = TextEditingController();
+  final ScrollController _scrollController = ScrollController();
+
   final List<String> _chatMessages = <String>[
     'Привет, чем я могу тебе помочь?',
   ];
@@ -30,6 +35,7 @@ class _ChatbotScreenState extends State<ChatbotScreen> {
         'В первые часы утра и в последние часы дня свет более мягкий и теплый, что может создавать лучшие условия для съемки.',
     'Какие эффекты сейчас в тренде?':
         'Сейчас очень популярны эффекты сепии, винтажа и черно-белого стиля.',
+    'дарова мудила': 'даров братан',
   };
 
   void _showResponse(String question) {
@@ -40,27 +46,74 @@ class _ChatbotScreenState extends State<ChatbotScreen> {
       _chatMessages.add('Чат-бот: $response');
       _controller.clear();
     });
+    _scrollController.animateTo(
+      _scrollController.position.maxScrollExtent,
+      duration: Duration(milliseconds: 300),
+      curve: Curves.easeOut,
+    );
   }
 
- @override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.lightBlue[100],
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.end,
         children: <Widget>[
           Expanded(
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 8.0),
               child: ListView.builder(
-                reverse: true,
+                controller: _scrollController,
+                reverse: false,
                 itemCount: _chatMessages.length,
                 itemBuilder: (BuildContext context, int index) {
                   final String message = _chatMessages[index];
-                  return ListTile(
-                    leading: message.startsWith('Вы')
-                        ? Icon(Icons.account_circle)
-                        : Icon(Icons.android),
-                    title: Text(message),
+                  return Column(
+                    children: [
+                      SizedBox(height: 8.0),
+                      InkWell(
+                        onTap: () {},
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(28.0),
+                            gradient: LinearGradient(
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                              colors: message.startsWith('Вы')
+                                  ? [
+                                      Colors.pink,
+                                      Colors.purple,
+                                    ]
+                                  : [
+                                      Colors.blue,
+                                      Colors.cyan,
+                                    ],
+                            ),
+                          ),
+                          padding: const EdgeInsets.all(8.0),
+                          child: ListTile(
+                            leading: message.startsWith('Вы')
+                                ? Icon(Icons.person)
+                                : Icon(Icons.android),
+                            title: Text(message,
+                                style: message.startsWith('Вы')
+                                    ? TextStyle(color: Colors.white)
+                                    : TextStyle(color: Colors.white)),
+                            onLongPress: () {
+                              Clipboard.setData(ClipboardData(text: message));
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content: Text("Текст скопирован"),
+                                  backgroundColor: Colors.green,
+                                  duration: Duration(seconds: 1),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                      ),
+                    ],
                   );
                 },
               ),
@@ -88,25 +141,52 @@ class _ChatbotScreenState extends State<ChatbotScreen> {
               ],
             ),
           ),
-          Container(
-            padding: const EdgeInsets.all(8.0),
-            child: Wrap(
-              spacing: 8.0,
-              runSpacing: 4.0,
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
               children: <Widget>[
                 CupertinoButton(
                   onPressed: () => _showResponse('Как делать хорошие фото?'),
-                  child: Text('Как делать хорошие фото?'),
+                  child: Text(
+                    'Как делать хорошие фото?',
+                    textAlign: TextAlign.center,
+                  ),
+                  color: Colors.deepPurpleAccent,
+                  borderRadius: BorderRadius.circular(20.0),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16.0,
+                    vertical: 8.0,
+                  ),
                 ),
+                const SizedBox(width: 8.0),
                 CupertinoButton(
                   onPressed: () => _showResponse(
                       'В какое время суток делать фото лучше всего?'),
-                  child: Text('В какое время суток делать фото лучше всего?'),
+                  child: Text(
+                    'В какое время суток делать фото лучше всего?',
+                    textAlign: TextAlign.center,
+                  ),
+                  color: Colors.deepPurpleAccent,
+                  borderRadius: BorderRadius.circular(20.0),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16.0,
+                    vertical: 8.0,
+                  ),
                 ),
+                const SizedBox(width: 8.0),
                 CupertinoButton(
                   onPressed: () =>
                       _showResponse('Какие эффекты сейчас в тренде?'),
-                  child: Text('Какие эффекты сейчас в тренде?'),
+                  child: Text(
+                    'Какие эффекты сейчас в тренде?',
+                    textAlign: TextAlign.center,
+                  ),
+                  color: Colors.deepPurpleAccent,
+                  borderRadius: BorderRadius.circular(20.0),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16.0,
+                    vertical: 8.0,
+                  ),
                 ),
               ],
             ),
